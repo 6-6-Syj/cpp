be38a1012af298df2906ad0e713fb13535d41ee3
6-6-Syj <magand.jessy@outlook.com>
2025-10-26 00:18:35 +0200

    tests

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,14 +6,20 @@
 /*   By: jmagand <jmagand@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 22:02:51 by jmagand           #+#    #+#             */
-/*   Updated: 2025/10/25 22:18:40 by jmagand          ###   ########.fr       */
+/*   Updated: 2025/10/26 00:18:18 by jmagand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main(void)
+int main(int ac, char **av)
 {
-    std::cout << "Hello World!" << std::endl;
+    std::string output;
+    
+    if (ac == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
+    }
     return (0);
 }
