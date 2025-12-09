b25968346e722600addf1be9a436446f94c931d1
6-6-Syj <magand.jessy@outlook.fr>
2025-12-09 15:48:19 +0100

    ex00 done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: jmagand <jmagand@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 22:02:51 by jmagand           #+#    #+#             */
-/*   Updated: 2025/10/26 00:18:18 by jmagand          ###   ########.fr       */
+/*   Updated: 2025/12/09 15:44:17 by jmagand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,24 @@
 
 int main(int ac, char **av)
 {
-    std::string output;
-    
+    int i, j;
+    char c;
+
+    j = 0;
     if (ac == 1)
     {
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
         return (0);
+    }
+	i = 0;
+    while (++i < ac)
+    {
+		j = 0;
+        while (av[i][j])
+        {
+            c = toupper(av[i][j++]);
+        	std::cout << c;
+        }
     }
     return (0);
 }
