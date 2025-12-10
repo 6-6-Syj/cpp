d090cbe23c3919981b8b19b1ad0904917721d192
6-6-Syj <magand.jessy@outlook.fr>
2025-12-10 17:02:24 +0100

    having timestamps, index, amout

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,11 +6,13 @@
 /*   By: jmagand <jmagand@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 18:29:02 by jmagand           #+#    #+#             */
-/*   Updated: 2025/12/10 15:03:43 by jmagand          ###   ########.fr       */
+/*   Updated: 2025/12/10 17:01:29 by jmagand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <iostream>
+#include <ctime>
 
 Account::Account () {}
 
@@ -21,7 +23,9 @@ Account::Account (int initial_deposit) {
 Account::~Account() {}
 
 void	Account::displayAccountsInfos() {
-	
+	_displayTimestamp();
+	// std::cout << "index:" << _accountIndex << ";amount:" << _amount << std::endl;
+	// std::cout << "accounts:" << "total:" << "deposits:" << "withdrawals:" << std::endl;
 }
 
 void	Account::makeDeposit(int deposit) {
@@ -29,11 +33,14 @@ void	Account::makeDeposit(int deposit) {
 }
 
 void	Account::displayStatus() const {
-	
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount 
+	<< ";deposits:" << this->_nbDeposits << ";withdrawals:" << this->_nbWithdrawals << std::endl;
 }
 
 void	Account::_displayTimestamp() {
-	
+	std::time_t seconds = std::time(NULL);
+	std::cout << "[" << seconds << "] ";
 }
 
 bool	Account::makeWithdrawal(int withdrawal) {
