7880f90b430788b327a837ec76e17286b1a662dc
6-6-Syj <magand.jessy@outlook.fr>
2026-04-17 12:41:40 +0200

    starting

--- ex00/include/easyfind.h
+++ ex00/include/easyfind.h
@@ -1,38 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   template.h                                         :+:      :+:    :+:   */
+/*   easyfind.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmagand <jmagand@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/16 11:41:42 by jmagand           #+#    #+#             */
-/*   Updated: 2026/04/16 11:41:44 by jmagand          ###   ########.fr       */
+/*   Created: 2026/04/17 12:36:28 by jmagand           #+#    #+#             */
+/*   Updated: 2026/04/17 12:38:59 by jmagand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 template <typename T>
-T max(T a, T b)
-{
-	if (a > b)
-		return a;
-	return b;
-}
-
-template <typename T>
-T min(T a, T b)
-{
-	if (a < b)
-		return a;
-	return b;
-}
-
-template <typename T>
-void swap(T &a, T &b)
-{
-	T tmp(a);
-
-	a = b;
-	b = tmp;
-}
+void easyfind(T x, int y);
