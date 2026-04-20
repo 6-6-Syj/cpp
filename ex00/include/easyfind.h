4a2f65bc6104a9e0254d23f49a9cdb890076a25c
6-6-Syj <magand.jessy@outlook.fr>
2026-04-20 09:04:43 +0200

    need to implement tests

--- ex00/include/easyfind.h
+++ ex00/include/easyfind.h
@@ -6,14 +6,20 @@
 /*   By: jmagand <jmagand@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 12:36:28 by jmagand           #+#    #+#             */
-/*   Updated: 2026/04/17 12:50:48 by jmagand          ###   ########.fr       */
+/*   Updated: 2026/04/20 09:04:11 by jmagand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include <string>
+
 template <typename T>
 typename T::iterator easyfind(T container, int value)
 {
+	typename T::iterator it = std::find(container.begin(), container.end(), value);
 	
+	if (it == container.end())
+		throw std::out_of_range("Value haven\'t been found");
+	return it;
 }
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,15 +6,30 @@
 /*   By: jmagand <jmagand@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 11:12:20 by jmagand           #+#    #+#             */
-/*   Updated: 2026/04/17 12:39:22 by jmagand          ###   ########.fr       */
+/*   Updated: 2026/04/20 09:03:04 by jmagand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
 #include "easyfind.h"
+#include <vector>
+#include <list>
 
 int main(void)
 {
+    std::vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(-5);
+    vec.push_back(3);
+    vec.push_back(7);
     
+    std::list<int> lst;
+    lst.push_back(10);
+    lst.push_back(20);
+    lst.push_back(30);
+    lst.push_back(40);
+
+    easyfind(vec, -5);
+    easyfind(lst, 40);
+
     return 0;
 }
